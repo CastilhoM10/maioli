1b0b047d162fe4c92d9a400f5681cd1a3520d750
Matheus Henrique de Castilho <mathews.castilho@gmail.com>
2026-03-17 17:12:03 -0300

    atualização 02

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,39 @@
 const toggle = document.querySelector(".menu-toggle")
 const nav = document.querySelector(".nav-links")
-const icon = toggle.querySelector("i")
 const overlay = document.querySelector(".menu-overlay")
+const icon = toggle.querySelector("i")
 
-toggle.addEventListener("click", ()=>{
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active")
+    overlay.classList.toggle("active")
 
-nav.classList.toggle("active")
-overlay.classList.toggle("active")
-
-if(nav.classList.contains("active")){
-icon.classList.remove("fa-bars")
-icon.classList.add("fa-x")
-}else{
-icon.classList.remove("fa-x")
-icon.classList.add("fa-bars")
-}
-
+    if(nav.classList.contains("active")){
+        icon.classList.remove("fa-bars")
+        icon.classList.add("fa-x")
+    } else {
+        icon.classList.remove("fa-x")
+        icon.classList.add("fa-bars")
+    }
 })
 
-overlay.addEventListener("click", ()=>{
+overlay.addEventListener("click", () => {
+    nav.classList.remove("active")
+    overlay.classList.remove("active")
 
-nav.classList.remove("active")
-overlay.classList.remove("active")
+    icon.classList.remove("fa-x")
+    icon.classList.add("fa-bars")
+})
 
-icon.classList.remove("fa-x")
-icon.classList.add("fa-bars")
+/* FECHAR AO CLICAR NO LINK */
 
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active")
+        overlay.classList.remove("active")
+
+        icon.classList.remove("fa-x")
+        icon.classList.add("fa-bars")
+    })
 })
 
 function abrirWhatsapp(){
